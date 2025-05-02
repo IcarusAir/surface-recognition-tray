3d54f357b4d9a3bdca1e9e16316abab9adb60681
IcarusAir <nicholasbmair@gmail.com>
2025-05-02 10:02:58 -0400

    Core Operations Test 0

--- OpenCV-test/opencv-core.py
+++ OpenCV-test/opencv-core.py
@@ -8,14 +8,28 @@
 import cv2 as cv
 import numpy as np
 
+def getCoords(event,x,y,flags,param):
+    if (event == cv.EVENT_LBUTTONDOWN):
+        print("X pos: ",x)
+        print("Y pos: ",y)
+
 test = 0
 
 if (test == 0):
     # Image Operations
     img = cv.imread('../test-data/elephant.jpg')
+    assert img is not None, "file could not be read, check with os.path.exists()"
 
+    # Get Eyes ROI
+    eyes = img[190:250, 300:400]
+    img[100:160, 530:630] = eyes
+
+    img[:,:,0:2] = 0
+
+    # Show Image
     cv.namedWindow('image')
+    cv.setMouseCallback('image',getCoords)
     cv.imshow('image',img)
     while True:
-        if cv.waitKey(1) == ord('q'):
+        if (cv.waitKey(20)==ord('q')):
             break
